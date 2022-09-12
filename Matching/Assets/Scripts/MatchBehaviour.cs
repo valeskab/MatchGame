a02a62bceb4685839d9d7b5998b79c4a1959310f
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-09-12 02:41:48 -0600

    Save Matching Game Progress

--- Matching/Assets/Scripts/MatchBehaviour.cs
+++ Matching/Assets/Scripts/MatchBehaviour.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehaviour : MonoBehaviour
 {
     public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent;
     private void OnTriggerEnter(Collider other)
     {
         var tempObj = other.GetComponent<IDContainerBehaviour>();
@@ -12,7 +14,11 @@ public class MatchBehaviour : MonoBehaviour
         var otherID = tempObj.idObj;
         if (otherID == idObj)
         {
-            Debug.Log("Matched");
+            matchEvent.Invoke();
+        }
+
+        {
+            noMatchEvent.Invoke();
         }
     }
 }
