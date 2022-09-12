6a57064e39cf1abbe9bbf28a551f5eb22ea3c277
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-09-11 23:42:40 -0600

    MatchBehaviour script fixed

--- Matching/Assets/Scripts/MatchBehaviour.cs
+++ Matching/Assets/Scripts/MatchBehaviour.cs
@@ -5,7 +5,11 @@ public class MatchBehaviour : MonoBehaviour
     public ID idObj;
     private void OnTriggerEnter(Collider other)
     {
-        var otherID = other.GetComponent<IDContainerBehaviour>().idObj;
+        var tempObj = other.GetComponent<IDContainerBehaviour>();
+        if (tempObj == null)
+            return;   // If the object is Null (if theres not an object there) its going to return meaning it's not gonna run the code below
+            
+        var otherID = tempObj.idObj;
         if (otherID == idObj)
         {
             Debug.Log("Matched");
