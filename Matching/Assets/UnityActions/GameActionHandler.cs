6b13d54877397c1cead2bdb594a4235a1cf529f8
Valeska <valeskabagaric@hotmail.com>
2022-09-27 17:38:32 -0600

    Fixed Game, Added Game Action scripts

---
+++ Matching/Assets/UnityActions/GameActionHandler.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    public UnityEvent onRaiseEvent;
+    private void Start()
+    {
+        gameActionObj.raise += Raise;
+    }
+
+    private void Raise()
+    {
+        onRaiseEvent.Invoke();
+    }
+}
